fa8ea008db7901c1218b858b167a905342edff76
Samuel Breckenridge <samuelbreckenridge1@gmail.com>
2025-02-10 15:46:56 -0500

    Implement live variables data flow analysis skeleton (no transfer function)

--- l4/data_flow.py
+++ l4/data_flow.py
@@ -3,20 +3,44 @@ import sys
 
 from cfg import build_cfg, form_basic_blocks
 
-def live_variables(blocks: list[list[dict]], cfg: dict) -> dict:
+
+def _merge_sets(sets: list[set]) -> set:
+    """Return a set that is the union of the input list of sets"""
+    merged = set()
+    for s in sets:
+        merged.union(s)
+    return merged
+
+
+def _live_vars_transfer(block: list[dict], out_vars: set) -> set:
+    """Given a block output compute the block input using the transfer function for live variable data flow"""
+    raise NotImplementedError
+
+
+def live_variables(blocks: list[list[dict]], cfg: dict) -> tuple[dict, dict]:
     # Maps block to in set of live vars
-    block_in : dict[int, set] = {b: set() for b in range(len(blocks))}
+    block_in : dict[int, set[str]] = {b: set() for b in range(len(blocks)+1)}
     # Maps block to out set of live vars
-    block_out: dict[int, set] = {len(blocks): set()}
+    block_out: dict[int, set[str]] = {len(blocks): set()}
 
     # Reverse cfg to obtain predecessor maps
-    pred_cfg = {b: [] for b in range(len(blocks)+1)}
+    pred_cfg = {i: [] for i in range(len(blocks)+1)}
     for k, v in cfg.items():
-        for b in v:
-            pred_cfg[b].append(k)
+        for i in v:
+            pred_cfg[i].append(k)
 
-    worklist = list(range(len(blocks)))
-    return {}
+    worklist = set(range(len(blocks)))
+    while worklist:
+        i = worklist.pop()
+        block_out[i] = _merge_sets([block_in[s] for s in cfg[i]])
+        orig_block_in = block_in[i]
+        block_in[i] = _live_vars_transfer(blocks[i], block_out[i])
+        if block_in[i] != orig_block_in:
+            # block_in changed, add predecessors to worklist
+            worklist.union(set(pred_cfg[i]))
+
+    return block_in, block_out
+
 
 if __name__ == "__main__":
     program = json.load(sys.stdin)
