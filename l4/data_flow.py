b593d11c4b35259e2f93f7422297754d1b0a6e86
Samuel Breckenridge <samuelbreckenridge1@gmail.com>
2025-02-10 15:26:51 -0500

    Set up live variables data flow analysis function

--- l4/data_flow.py
+++ l4/data_flow.py
@@ -3,13 +3,24 @@ import sys
 
 from cfg import build_cfg, form_basic_blocks
 
-def live_variables(blocks: list[dict], cfg: dict) -> dict:
-    """TODO"""
-    raise NotImplementedError
+def live_variables(blocks: list[list[dict]], cfg: dict) -> dict:
+    # Maps block to in set of live vars
+    block_in : dict[int, set] = {b: set() for b in range(len(blocks))}
+    # Maps block to out set of live vars
+    block_out: dict[int, set] = {len(blocks): set()}
+
+    # Reverse cfg to obtain predecessor maps
+    pred_cfg = {b: [] for b in range(len(blocks)+1)}
+    for k, v in cfg.items():
+        for b in v:
+            pred_cfg[b].append(k)
+
+    worklist = list(range(len(blocks)))
+    return {}
 
 if __name__ == "__main__":
     program = json.load(sys.stdin)
     for func in program["functions"]:
         bbs = form_basic_blocks(func)
         c = build_cfg(bbs)
-
+        lvs = live_variables(bbs, c)
