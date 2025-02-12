1da3446bc099d86bef587986f91b7e1fb25d8f5e
Ernest Ng <eyn5@cornell.edu>
2025-02-12 12:12:18 -0500

    Some helper functions for reaching_defs

--- l4/reaching_defs.py
+++ l4/reaching_defs.py
@@ -1,14 +1,32 @@
+# TODO: maybe switch to const prop? (its supposed to be easier)
+
 import json
 import sys
-from typing import List, Dict
+from typing import List, Dict, Set
 
 from cfg import build_cfg, form_basic_blocks
 
 def reaching_defs_transfer():
     raise NotImplementedError
 
-def reaching_defs(blocks: List[List[Dict]], _cfg: Dict): 
-    # Map each block to the in set of reaching defs (initially the mepty set)
+def get_defs(block: List[Dict]) -> Set[str]:
+    """Returns the set of variables that were defined in a block"""
+    defns = set()
+    for instr in block:
+        if instr["dest"]:
+            defns.add(instr["dest"])
+    return defns
+
+
+def get_kills(_block: List[Dict], _existing_defns: Set[str]) -> Set[str]:
+    """Returns the set of kills in a block"""
+    pass 
+
+def reaching_defs(blocks: List[List[Dict]], cfg: Dict): 
+    entry = cfg[0]
+
+
+    # Map each block to the in set of reaching defs (initially the empty set)
     block_in = { b : set() for b in blocks }
 
     # Map each block to the out set of reaching defs 
