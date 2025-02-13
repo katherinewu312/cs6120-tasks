cbb7ba5941b14e75985742efc09f03e4e0fd73d0
Ernest Ng <eyn5@cornell.edu>
2025-02-12 21:52:32 -0500

    Rename reaching_defs to const_prop

--- l4/const_prop.py
+++ l4/const_prop.py
@@ -1,7 +1,7 @@
-# TODO: maybe switch to const prop? (its supposed to be easier)
 
 import json
 import sys
+from copy import deepcopy
 from typing import List, Dict, Set
 
 from cfg import build_cfg, form_basic_blocks
@@ -9,20 +9,21 @@ from cfg import build_cfg, form_basic_blocks
 def reaching_defs_transfer():
     raise NotImplementedError
 
-def get_defs(block: List[Dict]) -> Set[str]:
-    """Returns the set of variables that were defined in a block"""
-    defns = set()
+def const_prop_transfer(block: List[Dict], in_dict: Dict):
+    out_dict = deepcopy(in_dict)
     for instr in block:
-        if instr["dest"]:
-            defns.add(instr["dest"])
-    return defns
-
-
-def get_kills(_block: List[Dict], _existing_defns: Set[str]) -> Set[str]:
-    """Returns the set of kills in a block"""
+        if "dest" in instr and "args" in instr:
+            dest = instr["dest"]
+            args = instr["args"]
+            if args in in_dict:
+                in_dict[dest] = pass # TODO: figure out what to do 
+            else:
+                in_dict[dest] = None 
     pass 
 
-def reaching_defs(blocks: List[List[Dict]], cfg: Dict): 
+
+
+def const_prop(blocks: List[List[Dict]], cfg: Dict): 
     entry = cfg[0]
 
 
