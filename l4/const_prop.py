261652e68a168a4e0155b25d0ab734192a99d27c
Ernest Ng <eyn5@cornell.edu>
2025-02-13 13:54:33 -0500

    Binary merge function for const prop

--- l4/const_prop.py
+++ l4/const_prop.py
@@ -6,8 +6,24 @@ from typing import List, Dict, Set
 
 from cfg import build_cfg, form_basic_blocks
 
-def reaching_defs_transfer():
-    raise NotImplementedError
+def const_prop_merge(d1: Dict, d2: Dict) -> Dict:
+    """Merge function: takes the union of two dictionaries. Any keys that are 
+    mapped to different values in `d1` and `d2` are mapped to `None` in the 
+    output dictionary.
+    """
+
+    merged_dict = d1 | d2
+    overlapping_keys = set(d1.keys()).intersection(set(d2.keys))
+    keys_with_diff_values = { k for k in overlapping_keys if d1[k] != d2[k] }
+    if len(keys_with_diff_values) == 0:
+        return merged_dict
+    else:
+        # Any variables that are mapped to different values in `d1` & `d2`
+        # should just be mapped to None 
+        for k in keys_with_diff_values:
+            merged_dict[k] = None 
+        return merged_dict
+
 
 def const_prop_transfer(block: List[Dict], in_dict: Dict) -> Dict:
     """Transfer function for constant propagation
