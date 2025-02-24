884cc3c7f0079c854b918066e8984e264fbc3d93
Ernest Ng <eyn5@cornell.edu>
2025-02-24 18:26:32 -0500

    Simplify dominance frontier code

--- l5/dominance_frontier.py
+++ l5/dominance_frontier.py
@@ -2,9 +2,8 @@
 import json
 import sys
 
-from cfg import form_basic_blocks, build_cfg
+from cfg import form_basic_blocks, build_cfg, get_pred_cfg
 from dominators import get_dominators
-from dominance_tree import get_dominance_tree
 from typing import List, Dict, Set
 
 # ---------------------------------------------------------------------------- #
@@ -34,29 +33,14 @@ type CFG = Dict[Idx, List[Idx]]
 # ---------------------------------------------------------------------------- #
 
 
-def get_immediate_dominators(cfg: CFG) -> Dict[Idx, Set[Idx]]:
-    """Computes the immediate dominators for each node in a CFG"""
-
-    # Note: the immediate dominators are just the children of each node
-    # in the dominator tree (here we just need to convert the list
-    # of children in `dom_tree` to a set)
-
-    dom_tree = get_dominance_tree(cfg)
-    idoms = {v: set(dom_tree[v]) for v in cfg}
-    return idoms
-
-
 def get_dominance_frontier(cfg: CFG):
     """Computes the dominance frontier for every node in a CFG."""
 
+    # Predecessors
+    preds = get_pred_cfg(cfg)
+
     # Dominator map (maps each `v` -> set of blocks that dominate node `v`)
     doms: Dict[Idx, Set[Idx]] = get_dominators(cfg)
-
-    # Dominator tree
-    dom_tree: Dict[Idx, List[Idx]] = get_dominance_tree(cfg)
-
-    # Immediate dominators
-    imm_doms = get_immediate_dominators(cfg)
 
     # Dominance frontier
     df: Dict[Idx, Set[Idx]] = {v: set() for v in cfg}
@@ -65,31 +49,31 @@ def get_dominance_frontier(cfg: CFG):
 
     def dominates(x: Idx, y: Idx) -> bool:
         """`dominates(x, y)` indicates whether `x` dominates `y`"""
-        return (x in doms[y])
+        return x in doms[y]
 
-    def idom(x: Idx, y: Idx) -> bool:
-        """`idom(x, y)` indicates whether `x` *immediately* dominates `y`"""
-        return (y in imm_doms[x])
-
-    def sdom(x: Idx, y: Idx) -> bool:
-        """`sdom(x, y)` indicates whether `x` *strictly* dominates `y`"""
+    def strictly_dominates(x: Idx, y: Idx) -> bool:
+        """`strictly_dominates(x, y)` indicates
+        whether `x` *strictly* dominates `y`"""
         result = (x != y) and dominates(x, y)
         return result
-    
+
     # ---------------------- Populate dominance frontier --------------------- #
 
-    for x, succs in cfg.items():
-        for y in succs:
-            # If `x` doesn't immediately dominate `y`, add `y` to `x`'s DF
-            if not idom(x, y):
-                df[x].add(y)
+    # `a`'s dominance frontier contains `b` if `a` doesn't strictly dominate `b`
+    # and `a` dominates some predecessor of `b`
+    for a, succs in cfg.items():
+        df[a] = {
+            b
+            for b in succs
+            if any(
+                {
+                    pred
+                    for pred in preds[b]
+                    if (not strictly_dominates(a, b)) and dominates(a, pred)
+                }
+            )
+        }
 
-        # For each of `x`'s children `z` in the dominator tree,
-        # check `z`'s DF and add each node in `df[z]` that isn't strictly dominated by `x`
-        for z in dom_tree[x]:
-            for y in df[z]:
-                if not sdom(x, y):
-                    df[x].add(y)
     return df
 
 
