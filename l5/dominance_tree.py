efb5c3b9b53ed371ce6569f838ecbee2e5023ab7
Katherine Wu <katherinewu312@yahoo.com>
2025-02-24 20:31:26 -0500

    some minor formatting

--- l5/dominance_tree.py
+++ l5/dominance_tree.py
@@ -8,25 +8,15 @@ from dominators import get_dominators
 
 def get_dominance_tree(doms: dict[int,set[int]], cfg: dict[int,list[int]]) -> dict[int, list[int]]:
     """Computes the dominance tree for a given CFG. 
-    For this tree, each node's children are those nodes it immediately dominates."""
-    dom_tree = {k: [] for k in doms}
-    rev_doms = {v: {k for k, s in doms.items() if v in s} for v in set.union(*doms.values())}
-    for vertex in rev_doms:
+    For this tree, each node's children are those nodes it immediately dominates.
+    Returns a mapping of vertex -> its successors in the dominance tree."""
+    dom_tree = dict()
+    reverse_doms = {v: {k for k, s in doms.items() if v in s} for v in set.union(*doms.values())}
+    for vertex in reverse_doms:
         # get the immediate successors of this node
         # but don't include the very last block, the one after the exit block
-        dom_tree[vertex] = [item for item in (rev_doms[vertex] & set(cfg[vertex])) if item != len(rev_doms) - 1]
+        dom_tree[vertex] = [item for item in (reverse_doms[vertex] & set(cfg[vertex])) if item != len(reverse_doms) - 1]
     return dom_tree
-        
-    
-    '''
-    visited_nodes = set()
-    for vertex in cfg:
-        for successor in cfg[vertex]:
-            if successor not in visited_nodes and successor != len(cfg)-1:
-                dom_tree[vertex].append(successor)
-                visited_nodes.add(successor)    
-    return dom_tree
-    '''
 
 
 if __name__ == "__main__":
