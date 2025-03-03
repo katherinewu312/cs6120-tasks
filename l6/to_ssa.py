806e2aeaa89361d92d2c6e55bd2d18120710c996
Samuel Breckenridge <samuelbreckenridge1@gmail.com>
2025-03-03 16:26:08 -0500

    wip

--- l6/to_ssa.py
+++ l6/to_ssa.py
@@ -1,12 +1,17 @@
 import json
 import sys
 
-from cfg import form_basic_blocks, add_entry_block, build_cfg
+from cfg import form_basic_blocks, build_cfg, get_pred_cfg
+
+
+def to_ssa(blocks: list[dict]) -> list[dict]:
+    cfg = build_cfg(blocks)
+    pred_cfg = get_pred_cfg(cfg)
+    pass
 
 if __name__ == "__main__":
     program = json.load(sys.stdin)
     for func in program["functions"]:
         bbs = form_basic_blocks(func)
-        c = build_cfg(bbs)
         print(func["name"])
         print(func)
