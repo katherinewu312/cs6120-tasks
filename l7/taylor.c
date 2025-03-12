3893156e178ac4e909cb5096d90871b2fab7ad77
Ernest Ng <eyn5@cornell.edu>
2025-03-12 17:37:19 -0400

    Fix typo in taylor.c

--- l7/taylor.c
+++ l7/taylor.c
@@ -1,7 +1,6 @@
 // Adapted from https://github.com/destro014/Numerical-Method-In-C/blob/master/taylors%20series.c
 
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
 
 long int factorial(int n) {
@@ -19,8 +18,8 @@ int main() {
   char c;
 
   printf("Consider the Taylor series of e^x minus the 1st term: -1 + x/1! + x^2/2! + x^3/3! + x^4/4! ... + x^x/x!");
-  printf("Enter the no. of sums to which you'd like the sum to be computed:");
-  scanf("%d", & x);
+  printf("Enter the no. of terms to which you'd like the sum to be computed:");
+  scanf("%d", &x);
   s = 0;
   for (i = 1; i <= x; i++) {
     s = s + ((float) pow(x, i) / (float) factorial(i));
