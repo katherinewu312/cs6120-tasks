4b7c170a449aeacfb2287329169b2fddb88c4318
Ernest Ng <eyn5@cornell.edu>
2025-03-12 17:27:51 -0400

    Update pass to work for both int & floating-point division

--- l7/skeleton/Skeleton.cpp
+++ l7/skeleton/Skeleton.cpp
@@ -32,6 +32,10 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
 
                         Type* denom_ty = denominator->getType();
 
+                        Constant* zero;
+                        Value* cmp_eq;
+                        Instruction* div_copy;
+
                         // Integer division
                         if (BO->getOpcode() == Instruction::SDiv || BO->getOpcode() == Instruction::UDiv) {
                             // Found a division instruction
@@ -42,63 +46,74 @@ struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
                             if (denom_ty->isIntegerTy()) {
                                 // Create an integer comparison instruction
                                 // which checks if the denominator is equal to 0
-                                Constant* zero = ConstantInt::get(denom_ty, 0);
-                                Value* cmp_eq = builder.CreateICmpEQ(denominator, zero, "is_zero");
-
-                                // Create a copy of the original division instruction
-                                // (this will only be executed if the denominator is non-zero)
-                                Instruction* div_copy = BO->clone();
-                                div_copy->setName(BO->getName() + "_copy");
-                                builder.Insert(div_copy);
-
-                                // Create a select instruction for safe division 
-                                // (LLVM select instructions are akin to C's ternary operators)
-                                // This means `cmp_eq ? zero : div_copy`
-                                Value* safe_div_select = builder.CreateSelect(cmp_eq, zero, div_copy, "safe_div_result");
-                               
-                                errs() << "Created new instructions:\n";
-                                cmp_eq->print(errs() , true);
-                                errs() << "\n";
-                                div_copy->print(errs() , true);
-                                errs() << "\n";
-                                safe_div_select->print(errs(), true);
-                                errs() << "\n";
-
-                                // Replace all uses of the original division instruction
-                                // with the safe division instruction created above
-                                for (auto &U : BO->uses()) {
-                                    // Fetch the entire instruction corresponding to the use
-                                    User *user = U.getUser();
-
-                                    errs() << "Original use:\n";
-                                    user->print(errs(), true);
-                                    errs() << "\n";
-
-                                    // Update the corresponding operand to use 
-                                    // the result of the safe division instead
-                                    user->setOperand(U.getOperandNo(), safe_div_select);
-
-                                    errs() << "Updated use:\n";
-                                    user->print(errs(), true);
-                                    errs() << "\n";
-                                }
-
-                                // Remove the original division instruction
-                                BO->eraseFromParent();
-
-                                // We modified the code, so no analyses are preserved
-                                return PreservedAnalyses::none();
+                                zero = ConstantInt::get(denom_ty, 0);
+                                cmp_eq = builder.CreateICmpEQ(denominator, zero, "is_zero");
                             } else {
                                 errs() << "we have an integer division but denominator doesn't have type int!\n";
-                            }                            
-                            
+                            }                                
                         } else if (BO->getOpcode() == Instruction::FDiv) {
                             // floating point division
-    
                             errs() << "Found floating-point division : \n";
                             I.print(errs(), true);
                             errs() << "\n";
+
+                            if (denom_ty->isFloatingPointTy()) {
+                                // Create an floating point comparison instruction
+                                // which checks if the denominator is equal to 0
+                                zero = ConstantFP::get(denom_ty, 0);
+
+                                // We use LLVM's *ordered equals* operator for FPs `FCmpOEQ`, 
+                                // as opposed to `FCmpUEQ` (unordered equals), 
+                                // since the former behaves more predictably with possibly null operands
+                                cmp_eq = builder.CreateFCmpOEQ(denominator, zero, "is_zero");
+                            } else {
+                                errs() << "We have FP division but denominator doesn't have a floating point type!\n";
+                            } 
                         }
+
+                        // Create a copy of the original division instruction
+                        // (this will only be executed if the denominator is non-zero)
+                        div_copy = BO->clone();
+                        div_copy->setName(BO->getName() + "_copy");
+                        builder.Insert(div_copy);
+
+                        // Create a select instruction for safe division 
+                        // (LLVM select instructions are akin to C's ternary operators)
+                        // This means `cmp_eq ? zero : div_copy`
+                        Value* safe_div_select = builder.CreateSelect(cmp_eq, zero, div_copy, "safe_div_result");
+                        
+                        errs() << "Created new instructions:\n";
+                        cmp_eq->print(errs() , true);
+                        errs() << "\n";
+                        div_copy->print(errs() , true);
+                        errs() << "\n";
+                        safe_div_select->print(errs(), true);
+                        errs() << "\n";
+
+                        // Replace all uses of the original division instruction
+                        // with the safe division instruction created above
+                        for (auto &U : BO->uses()) {
+                            // Fetch the entire instruction corresponding to the use
+                            User *user = U.getUser();
+
+                            errs() << "Original use:\n";
+                            user->print(errs(), true);
+                            errs() << "\n";
+
+                            // Update the corresponding operand to use 
+                            // the result of the safe division instead
+                            user->setOperand(U.getOperandNo(), safe_div_select);
+
+                            errs() << "Updated use:\n";
+                            user->print(errs(), true);
+                            errs() << "\n";
+                        }
+
+                        // Remove the original division instruction
+                        BO->eraseFromParent();
+
+                        // We modified the code, so no analyses are preserved
+                        return PreservedAnalyses::none();
                     }
                 }
             }
