02be55b18500a91c66592b605159da626ff7a21c
Samuel Breckenridge <samuelbreckenridge1@gmail.com>
2025-03-21 15:30:15 -0400

    Update LICM pass with more conditions for moving loop invariant instruction to preheader

--- l8/licm/licm.cpp
+++ l8/licm/licm.cpp
@@ -14,15 +14,6 @@ using namespace llvm;
 namespace {
 
 struct LICMPass : public PassInfoMixin<LICMPass> {
-    // library functions to look at:
-    // `Loop::hasLoopInvariantOperands`
-    // `LoopBase::getLoopPreheader`
-    // `Instruction::moveBefore`
-
-    // other helpful functions:
-    // `llvm::isSafeToSpeculativelyExecute`
-    // `BasicBlock::getTerminator`
-
     PreservedAnalyses run(Loop &L, LoopAnalysisManager &AM, LoopStandardAnalysisResults &AR, LPMUpdater &U) {
         errs() << "Inside a loop!\n";
         BasicBlock *preheader = L.getLoopPreheader();
@@ -34,8 +25,23 @@ struct LICMPass : public PassInfoMixin<LICMPass> {
             for (auto &BB : L.blocks()) {
                 // Iterate over the block's instructions
                 for (auto &I : *BB) {
-                    // Check if instruction is loop invariant and contains no side effects or undefined behavior
-                    if (L.hasLoopInvariantOperands(&I) && isSafeToSpeculativelyExecute(&I) && !I.mayReadOrWriteMemory()) {
+
+                    if (!isa<BinaryOperator>(&I)) {
+                        continue;   // Avoid more complicated instructions
+                    }
+
+                    bool defs_outside_loop = true;
+                    for (auto &O: I.operands()) {
+                        if (isa<Constant>(&O)) {
+                            continue;
+                        }
+                        if (!L.contains(dyn_cast<Instruction>(&O)->getParent())) {
+                            continue;
+                        }
+                        defs_outside_loop = false;
+                    }
+
+                    if (defs_outside_loop) {
                         loop_inv_instrs.push_back(&I);
                         converged = false;
                     }
@@ -43,6 +49,23 @@ struct LICMPass : public PassInfoMixin<LICMPass> {
             }
             // Iterate through all loop invariant instructions and move them to preheader
             for (auto *I : loop_inv_instrs) {
+                if (!isSafeToSpeculativelyExecute(I)) {
+                    errs() << "Loop invariant instruction " << *I << " has side effects. Checking it dominates all exits"<< "\n";
+                    SmallVector<BasicBlock *, 8> ExitBlocks;
+                    auto DT = DominatorTree(*I->getParent()->getParent());
+                    L.getExitBlocks(ExitBlocks);
+                    bool dominates_exit_blocks = true;
+                    for (auto &B : ExitBlocks) {
+                        if (DT.dominates(I->getParent(), B)) {
+                            dominates_exit_blocks = false;
+                            break;
+                        }
+                    }
+                    if (!dominates_exit_blocks) {
+                        errs() << "Loop invariant instruction does not dominate all exits. Will not move to preheader.";
+                        continue;
+                    }
+                }
                 errs() << "Moving loop invariant instruction to preheader: " << *I << "\n";
                 I->moveBefore(preheader->getTerminator());
             }
