72d014ccd954032aa790345315d49475e9fbe25e
Ernest Ng <eyn5@cornell.edu>
2025-04-19 15:49:52 -0400

    Add Adrian's advice from the L12 video

--- l12/brili.ts
+++ l12/brili.ts
@@ -447,6 +447,9 @@ function evalCall(instr: bril.Operation, state: State): Action {
  * instruction or "end" to terminate the function.
  */
 function evalInstr(instr: bril.Instruction, state: State): Action {
+
+  // TODO: do something with `instr` here (e.g. `console.log(JSON.stringify(instr)));`)
+
   console.log(JSON.stringify(instr));
   state.icount += BigInt(1);
 
